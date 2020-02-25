c4f31abd9aa7add53c881a34901c712da7760ba5
LeeWan <etlyc2@naver.com>
2020-02-25 12:11:02 +0900

    Add_DefensiveScout
    정찰병(수비적) 틀 추가

---
+++ Assets/Scripts/WAR_MAP/Enemey/Class/Defensive_Scout.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Defensive_Scout : Enemy_Manager
+{
+    public override bool Attack()
+    {
+        return base.Attack();
+    }
+
+    public override bool CoverCheck(Unit_Manager _Unit)
+    {
+        base.CoverCheck(_Unit);
+    }
+}
